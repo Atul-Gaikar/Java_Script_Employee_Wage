d3efd256700f5663a3a2dd60e294a8e76c53a85f
Atul <atulgaikarag9@gmail.com>
2021-07-30 13:25:42 +0530

    Added : Employee Is Present Or Absent

--- employeewage.js
+++ employeewage.js
@@ -1,1 +1,9 @@
-console.log("Welcome To Employee Wage");
+const Is_Absent = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == Is_Absent) {
+    console.log("Employee Is Absent");
+    return;
+}
+else {
+    console.log("Employee Is Present");
+}
