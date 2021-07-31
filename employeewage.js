6871d1c233ed5f9c4d296967ed1765c4816a34dd
Atul <atulgaikarag9@gmail.com>
2021-07-31 13:08:07 +0530

    Added : Refactor The Code

--- employeewage.js
+++ employeewage.js
@@ -30,3 +30,19 @@ switch (empCheck) {
 
 let empWage = empHrs * WAGE_PER_HOURS;
 console.log("Emp Wage: " + empWage);
+
+//UC3-RefactorTheCode
+
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+
+    }
+}
+empHrs = getWorkingHours(empCheck);
+console.log("Emp Wage: " + empWage);
